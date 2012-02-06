9a43e222d087abf7ff1d75ba31ae591ae3293bc5
Rodrigo Sampaio Vaz <rodrigo.vaz@gmail.com>
2012-02-06 02:50:45 -0200

    fixed a few bugs and updated bootstrap to version 2.0

--- bricklayer/rest.py
+++ bricklayer/rest.py
@@ -169,20 +169,23 @@ class Check(cyclone.web.RequestHandler):
 
 class Group(cyclone.web.RequestHandler):
     def post(self, *args):
-        if len(args) > 0:
-            name = args[0]
-            group = Groups(name)
-            for key, value in self.request.arguments.iteritems():
-                if key in ("repo_addr", "repo_user", "repo_passwd"):
-                    setattr(group, key, value[0])
-            group.save()
-        else:
-            group = Groups(self.get_argument('name'))
-            group.repo_addr = self.get_argument('repo_addr')
-            group.repo_user = self.get_argument('repo_user')
-            group.repo_passwd = self.get_argument('repo_passwd')
-            group.save()
-
+        try:
+            if len(args) > 0:
+                name = args[0]
+                group = Groups(name)
+                for key, value in self.request.arguments.iteritems():
+                    if key in ("repo_addr", "repo_user", "repo_passwd"):
+                        setattr(group, key, value[0])
+                group.save()
+            else:
+                group = Groups(self.get_argument('name'))
+                group.repo_addr = self.get_argument('repo_addr')
+                group.repo_user = self.get_argument('repo_user')
+                group.repo_passwd = self.get_argument('repo_passwd')
+                group.save()
+            self.write(cyclone.escape.json_encode({'status': 'ok'}))
+        except Exception, e:
+            self.write(cyclone.escape.json_encode({'status': 'fail', 'error': str(e)}))
 
     def get(self, *args):
         if len(args) > 1:
--- web/js/bricklayer.js
+++ web/js/bricklayer.js
@@ -7,12 +7,16 @@ $(function(){
         
         $("#" + section + "-" + name + "-details").html($.mustache(template, { builds: builds, details:details }));
 
-        $("table[class*=tablesorter]").tablesorter()
+        $("table[class*=tablesorter]").tablesorter();
+
         $("a[id='build-log-show']").click(function () {
                 var build_id = $(this).attr("build_id");
                 show_log(name, build_id);
                 $.doTimeout('refresh-log', 2000, function () {
-                    show_log(name, build_id);
+
+                    if ($("#auto-refresh:checked").length > 0) {
+                        show_log(name, build_id);
+                    }
                     return true;
                 });
         });
@@ -35,17 +39,19 @@ $(function(){
 
     function show_log(name, build_id) {
         var build_log = $.ajax({url: "/log/" + name + "/" + build_id, dataType: "json", async: false}).responseText;
-        $("#build-log-view").find("div.modal-body").html("<pre>" + build_log + "</pre>");
-        $('#build-log-view').find("div.modal-body").attr("scrollTop", $('#build-log-view').find("div.modal-body").attr("scrollHeight"));
+        $("#build-log-view").find("pre").html(build_log);
+        
         if ($("#build-log-view").css("display") == "none") 
             $("#build-log-view").modal("show");
-
-
-        $("a[id='refresh-button']").click(function() {
-            show_log(name, build_id); 
-        });
+        
+        $('#log-viewer')[0].scrollTop = $('#log-viewer')[0].scrollHeight;
+        
         $("#build-log-view").bind("hide", function () {
             $.doTimeout('refresh-log');
+        });
+
+        $("#close-button").click(function() {
+            $("#build-log-view").modal("hide");
         });
 
     }
@@ -67,7 +73,7 @@ $(function(){
                         return 0;
                 }) } ));
                 $("#create").click(function() {
-                        $("div[id="+ section +"-form]").modal("show");
+                        $("div[id="+ section +"-modal]").modal("show");
                 });
 
                 $("ul.nav").find("li").removeClass("active");
@@ -108,24 +114,26 @@ $(function(){
 
     }
    
-    $('div[id*="-form"]').each(function() {
+    $('div[id*="-modal"]').each(function() {
         /* init forms */
         var section = $(this).attr("id").split("-")[0];
+        
         $(this).modal({"backdrop": true});
+        $(this).modal("hide");
 
         $(this).find("#create-" + section + "-button").click(function() { 
-                    var data = $("div[id=" + section + "-form]").find("form").serialize();
+                    var data = $("div[id=" + section + "-modal]").find("form").serialize();
                     $.post("/"+ section, data, function() {
-                        $("#" + section +"-form").find("input").each(
+                        $("#create-" + section +"-form").find("input").each(
                             function () {
                                 $(this).val("");
                             });
-                        $("div[id="+ section +"-form]").dialog("close"); 
+                        $("div[id="+ section +"-modal]").modal("hide"); 
                         visit(section);
                     });
                 });
-
-        $(this).find("#cancel-" + section + "-button").click(function() { $(this).modal("hide"); });
+        
+        $(this).find("#cancel-" + section + "-button").click(function() { $("div[id="+ section +"-modal]").modal("hide"); });
     });
                 
     /*
@@ -138,6 +146,7 @@ $(function(){
     */
 
     $("#build-log-view").modal({'backdrop': true});
+    $("#build-log-view").modal("hide");
     
     $("ul.nav").find("li").each(function(){
         var menu_item = $(this);
@@ -147,5 +156,13 @@ $(function(){
         });
     });
     
+    $('#create-group-form').ajaxForm(function() { 
+            
+    });
+
+    $('#create-project-form').ajaxForm(function() { 
+        
+    });
+
     visit("project");
 });
